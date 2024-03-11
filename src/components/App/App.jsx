84649dad4d42b94d3326824fa06c7d26cf290f3a
Yurii <jarovski08@gmail.com>
2024-03-11 12:16:36 +0200

    project settings

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,3 @@
-import Profile from "../Profile/Profile.jsx";
-import userData from "../../userData.json";
-import FriendList from "../FriendList/FriendList.jsx";
-import friends from "../../friends.json";
-import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx";
-import transactions from "../../transactions.json";
-
 const App = () => {
   return (
     <>
