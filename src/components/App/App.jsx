ad33ff55164f0212e6244992f17cbda2204e0d16
Yurii <jarovski08@gmail.com>
2024-03-12 15:19:24 +0200

    set project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,5 @@
 const App = () => {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
